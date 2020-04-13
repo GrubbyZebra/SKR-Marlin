657ab663a302319a0a57503b513a1486b976a666
Chris <codiac2600@gmail.com>
2020-04-13 16:39:32 -0500

    BMG Commit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -735,7 +735,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 280 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 830 }
 
 /**
  * Default Max Feed Rate (mm/s)
