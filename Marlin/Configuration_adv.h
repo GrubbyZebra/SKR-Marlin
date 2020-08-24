52f99bb9d5818acbbee6aac92ca61735d7117760
Chris <codiac2600@gmail.com>
2020-08-24 13:03:58 -0500

    Rsense patch
    wrong rsense value switched from -0.11 to 0.22

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2216,7 +2216,7 @@
     #define X_CURRENT       500      // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  200      // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16      // 0..256
-    #define X_RSENSE          0.11
+    #define X_RSENSE          0.22
     #define X_CHAIN_POS      -1      // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
 
@@ -2232,7 +2232,7 @@
     #define Y_CURRENT       600
     #define Y_CURRENT_HOME  250
     #define Y_MICROSTEPS     16
-    #define Y_RSENSE          0.11
+    #define Y_RSENSE          0.22
     #define Y_CHAIN_POS      -1
   #endif
 
@@ -2248,7 +2248,7 @@
     #define Z_CURRENT       450
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
-    #define Z_RSENSE          0.11
+    #define Z_RSENSE          0.22
     #define Z_CHAIN_POS      -1
   #endif
 
@@ -2279,7 +2279,7 @@
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT      550
     #define E0_MICROSTEPS    32
-    #define E0_RSENSE         0.11
+    #define E0_RSENSE         0.22
     #define E0_CHAIN_POS     -1
   #endif
 
