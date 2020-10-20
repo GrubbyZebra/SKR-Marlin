72cfe43974ef4456b0b8136786708656383e6d55
Chris <codiac2600@gmail.com>
2020-10-20 17:46:32 -0500

    Init Commit
    Purple Bear w/ SKR turbo, PINDA, BMG Dragon

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  16.50
-    #define DEFAULT_Ki   1.16
-    #define DEFAULT_Kd  58.80
+    #define DEFAULT_Kp  22.28
+    #define DEFAULT_Ki   1.61
+    #define DEFAULT_Kd  77.07
   #endif
 #endif // PIDTEMP
 
@@ -541,7 +541,7 @@
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
   #define DEFAULT_bedKp 28.13
-  #define DEFAULT_bedKi 2.09
+  #define DEFAULT_bedKi 2.50
   #define DEFAULT_bedKd 483.07
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
